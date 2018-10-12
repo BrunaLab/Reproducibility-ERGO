0704c26a802c2bb60d23e187afb70a5e5bcf9cd8
Aariq <scottericr@gmail.com>
2018-10-12 15:28:59 -0400

    Demonstrated the extract function menu item.

--- Extract function demo.R
+++ Extract function demo.R
@@ -1,7 +1,11 @@
 x = c(10.1, 11.2, 9.1, 22.1)
 
-m <- mean(x)
-stdev <- sd(x)
-n <- length(x)
-sem <- stdev/sqrt(n)
-paste0(round(m, 2), " ± ", round(sem, 2))
+mySEM <- function(x) {
+  m <- mean(x)
+  stdev <- sd(x)
+  n <- length(x)
+  sem <- stdev/sqrt(n)
+  paste0(round(m, 2), " ± ", round(sem, 2))
+}
+
+mySEM(c(4, 5, 23, 2))
